dfa9ff0d63e4fc0b05296ee8aa1903b81f9e6f38
rk30207241300666@depi.eui.edu.eg <rk30207241300666@depi.eui.edu.eg>
2026-04-28 19:23:56 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
+import RegisterPage from "./pages/auth/RegisterPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RegisterPage />;
 }
 
 export default App;
