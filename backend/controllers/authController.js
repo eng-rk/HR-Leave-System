c7acf5bb6d3391361de46e76f37a7a2155d4de94
Rowan <rk30207241300666@depi.eui.edu.eg>
2026-05-02 17:14:10 +0300

    authController
    Removed forgetpwd and resetpwd functions from authController.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -73,57 +73,6 @@ const login=async(req, res)=>{
     })
   }
 }
-const forgetpwd = async (req, res) => {
-  try{
-    const emp = await User.findOne({ email: req.body.email })
-        if (!emp)
-            return res.status(404).json({
-                msg: "emp not found"
-            })
-            const token = jwt.sign(
-                { id: emp._id },
-                process.env.JWT_SECRET,
-                { expiresIn: "10m" }
-            );
-        
-            
-            emp.resetToken = token;
-            emp.resetTokenExpire = Date.now() + 10 * 60 * 1000;
-        
-        await emp.save();
-        const link = `http://localhost:8000/api/auth/resetpassword/${token}`;
-        res.json({ msg: "Reset link sent", link: link });
-  }
-         catch(error){
-    return res.status(500).json({
-      msg:"error in the user  controller ",
-      error:error.message
-    })
-  }
-    }
 
 
-const resetpwd = async (req, res) => {
-        const token = req.params.token;
-        const password = req.body.password;
-        let decoded;
-        try {
-            decoded = jwt.verify(token, process.env.JWT_SECRET);
-        } catch (err) {
-            return res.status(400).json({ msg: "Invalid or expired token" });
-        }
-        const emp = await User.findOne({
-            _id: decoded.id,
-            resetToken: token,
-            resetTokenExpire: { $gt: Date.now() }
-        })
-        if (!emp) { return res.status(400).json({ msg: "Token not valid" }); }
-        const newpwd = await bcrypt.hash(password, 10)
-        emp.password = newpwd;
-        emp.resetToken = undefined;
-        emp.resetTokenExpire = undefined;
-        await emp.save();
-        res.json({ msg: "Password reset successful " });
-    }
-
-module.exports = {register, login ,forgetpwd,resetpwd}
+module.exports = {register, login }
