4db3e47c76054423acc73830b53cc3734056179d
Hazem <abdelazez.hazem@gmail.com>
2026-05-02 20:37:25 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from "./pages/auth/RegisterPage";
 import LoginPage from "./pages/auth/LoginPage";
+import ApplyForm from "./Components/ApplyForm/ApplyForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Navigate to="/login" /> } />
+      <Route path="/form" element={<ApplyForm />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
 
+
 export default App;
+
+
